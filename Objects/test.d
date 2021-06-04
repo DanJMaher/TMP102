c3b02f7eabc9fb4d694d12c5f2a7e83b976c3a1a
Dan Maher <djm227@gmail.com>
2021-06-04 11:19:29 -0400

    removed test files

--- Objects/test.d
+++
@@ -1,9 +0,0 @@
-./objects/test.o: test.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\inc\hw_memmap.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\gpio.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\sysctl.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\pin_map.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\uart.h \
-  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\i2c.h tmp102.h
